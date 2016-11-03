a708341168f976495c2b63d3c45c39da95f2b495
Chiheb NeXus <chihebnexus@gmail.com>
2016-11-03 04:45:20 +0100

    3rd wave

--- dir_3/example2.go
+++ dir_3/example2.go
@@ -1,25 +1,26 @@
 package main
+
 import (
 	"fmt"
 )
 
-func main(){
-	tab := make([]int, 10)	// Create a slice of int
-	
-	for i:= range tab{
-	
-		if i % 2 == 0 {
-			continue	// Skip iteration
+func main() {
+	tab := make([]int, 10) // Create a slice of int
+
+	for i := range tab {
+
+		if i%2 == 0 {
+			continue // Skip iteration
 		}
-		
+
 		tab[i] = i << uint(i)
-		
-		if tab[i] > 2000{
-			break 		// Break iteration
+
+		if tab[i] > 2000 {
+			break // Break iteration
 		}
 	}
-	
-	for _, value := range tab{
+
+	for _, value := range tab {
 		fmt.Printf("%d\n", value)
 	}
 }
